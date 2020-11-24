f559fc947471391c5b933842b198535511d92094
YuriRamalho-Silva <yuri.ramalho.silva.16@gmail.com>
2020-11-24 13:26:57 -0300

    Validação de usuario cadastrado via hawk

--- app/src/main/java/br/com/projetointegrado/CadastroActivity.java
+++ app/src/main/java/br/com/projetointegrado/CadastroActivity.java
@@ -3,6 +3,9 @@ package br.com.projetointegrado;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.orhanobut.hawk.Hawk;
 
 public class CadastroActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class CadastroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+        Hawk.init(this).build();
+    }
+
+    public void salvar(View view) {
+        Hawk.put("tem_cadastro", true);
+        finish();
     }
 }
--- app/src/main/java/br/com/projetointegrado/LoginActivity.java
+++ app/src/main/java/br/com/projetointegrado/LoginActivity.java
@@ -5,13 +5,52 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.orhanobut.hawk.Hawk;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextView textViewNovoCadastro;
+    private Button buttonLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Hawk.init(this).build();
+
+        textViewNovoCadastro = findViewById(R.id.textViewNovoCadastro);
+        buttonLogin = findViewById(R.id.buttonLogin);
+
+
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        if (Hawk.contains("tem_cadastro")){
+            if (Hawk.get("tem_cadastro")){
+                habilitarLogin();
+            }else {
+                desabilitarLogin();
+            }
+        }else {
+            desabilitarLogin();
+        }
+    }
+
+    private void habilitarLogin() {
+        textViewNovoCadastro.setVisibility(View.GONE);
+        buttonLogin.setEnabled(true);
+        buttonLogin.setBackgroundColor(getColor(R.color.blue));
+    }
+
+    private void desabilitarLogin() {
+        textViewNovoCadastro.setVisibility(View.VISIBLE);
+        buttonLogin.setEnabled(false);
+        buttonLogin.setBackgroundColor(getColor(R.color.gray));
     }
 
     public void novoCadastro(View view) {
